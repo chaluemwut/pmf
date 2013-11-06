ee9a37119e9a1dd590341ad4cdadc5f2a8661016
chaluemwut <chaluemwut@hotmail.com>
2013-11-06 11:08:41 +0700

    add get save

--- pmf/src/com/nsc/pmf/PMF.java
+++ pmf/src/com/nsc/pmf/PMF.java
@@ -1,9 +1,21 @@
 package com.nsc.pmf;
 
+import java.util.Map;
+
 public class PMF {
 
+	public PMF(String clientId) {
+	}
+
+	public Map<String, String> get(String objectId) {
+		return null;
+	}
+
+	public void save(String objectId, Map<String, String> data) {
+
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
